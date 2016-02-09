408414885ee2a6f75dd7fb3ddf02e5418992d234
Eetu Laukka <eetu.laukka@student.oulu.fi>
2016-02-09 10:12:03 +0200

    Version1

--- test.py
+++ test.py
@@ -9,13 +9,19 @@ class TestSuite(unittest.TestCase):
     def test_one(self):
         app = FizzBuzz()
         self.failIf(app.calc(1) != 1)
-
+    def test_two(self):
+        app = FizzBuzz()
+        self.failIf(app.calc(3) != "Fizz")
+    def test_three(self):
+        app = FizzBuzz()
+        self.failIf(app.calc(5) != "Buzz")
+    def test_four(self):
+        app = FizzBuzz()
+        self.failIf(app.calc(15) != "FizzBuzz")
     def test_run(self):
         output = StringIO()
-
         app = FizzBuzz()
         app.run(100, output)
-
         self.failIf(len(output.getvalue().splitlines()) != 100  )
 
 def main():
